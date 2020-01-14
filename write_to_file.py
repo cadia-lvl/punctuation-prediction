472f08ac0c63e2cfbe06d6f1e9d7d8e41b22d8d8
helga-lvl <56829995+helga-lvl@users.noreply.github.com>
2020-01-14 14:07:49 +0000

    Update write_to_file.py

--- write_to_file.py
+++ write_to_file.py
@@ -1,37 +1,53 @@
-from process_text.py import process_line
+from process_text import process_line
+from sklearn.model_selection import train_test_split
+import sys
 
-# Get the contents of a file into the correct format for processing
-def read_file_string(src):
-    with open(src, 'r') as file:
-        lines = file.read()
-    splitLines = lines.split('\n')
-    return [x.split('\t') for x in splitLines]
+# We process the input text according to the processing script
 
-# Now we process the input text according to the previous script
-if __name__ == "__main__":
+try:
     if len(sys.argv) > 1:
-        file_string = read_file_string(sys.argv[1])
-    else:
+        float(sys.argv[1])
         print("There is no specified text to process")
+        sys.exit(0)
+except ValueError:
+    with open(sys.argv[1], 'r') as file:
+        lines = file.read()
+    file_string =  lines.split('\n')
 
-    processed_text = [process_line(elem) for elem in file_string]
+if not isinstance(file_string[0], str):
+    print("The input text needs to be a list of strings")
+    sys.exit(0)
 
-    # Write it to train, dev, and test files.
-    if len(sys.argv)  == 4:
-        train_text, tmp_text = train_test_split(processed_text, test_size=sys.argv[2], random_state=42)
-        dev_text, test_text = train_test_split(tmp_text, test_size=sys.argv[3] random_state=42)
-    else:
-        train_text, tmp_text = train_test_split(processed_text, test_size=0.2, random_state=42)
-        dev_text, test_text = train_test_split(tmp_text, test_size=0.5 random_state=42)
+print(f'Number of rows in file: {len(file_string)}')
 
-    with open("processed_text.train.txt", 'w', encoding='utf-8') as train_file:
-        for item in train_text:
-            train_file.write("%s\n" % item)
+# Punctuation marks processed
+processed_text = [process_line(elem) for elem in file_string]
 
-    with open("processed_text.dev.txt", 'w', encoding='utf-8') as dev_file:
-        for item in dev_text:
-            dev_file.write("%s\n" % item)
+# Save processed_text to file
+with open("processed_text.all.txt", 'w', encoding='utf-8') as all_file:
+    for item in processed_text:
+        all_file.write("%s\n" % item)
 
-    with open("processed_text.test.txt", 'w', encoding='utf-8') as test_file:
-        for item in test_text:
-            test_file.write("%s\n" % item)
+print("Done processing the text")
+
+# Write it to train, dev, and test files.
+if len(sys.argv)  > 3:
+    train_text, tmp_text = train_test_split(processed_text, test_size=float(sys.argv[2]), random_state=42)
+    dev_text, test_text = train_test_split(tmp_text, test_size=float(sys.argv[3]), random_state=42)
+else:
+    train_text, tmp_text = train_test_split(processed_text, test_size=0.2, random_state=42)
+    dev_text, test_text = train_test_split(tmp_text, test_size=0.5, random_state=42)
+
+with open("datadir/processed_text.train.txt", 'w', encoding='utf-8') as train_file:
+    for item in train_text:
+        train_file.write("%s\n" % item)
+
+with open("datadir/processed_text.dev.txt", 'w', encoding='utf-8') as dev_file:
+    for item in dev_text:
+        dev_file.write("%s\n" % item)
+
+with open("datadir/processed_text.test.txt", 'w', encoding='utf-8') as test_file:
+    for item in test_text:
+        test_file.write("%s\n" % item)
+
+print("Done saving files to data directory")
