c5fb9d22e575a507483757ed04bff8060042517a
helga-lvl <56829995+helga-lvl@users.noreply.github.com>
2020-01-14 14:57:24 +0000

    Update process_text.py

--- process_text.py
+++ process_text.py
@@ -1,5 +1,5 @@
 # Copyright 2016 Ottokar Tilk and Tanel Alumäe
-# The following code is available on: 
+# The following code is available on:
 # https://github.com/ottokart/punctuator2/blob/master/example/dont_run_me_run_the_other_script_instead.py
 # coding: utf-8
 
@@ -12,21 +12,23 @@ from io import open
 import re
 import sys
 
-nltk.download('punkt')
+nltk.download("punkt")
 
-NUM = '<NUM>'
+NUM = "<NUM>"
 
 EOS_PUNCTS = {".": ".PERIOD", "?": "?QUESTIONMARK", "!": "!EXCLAMATIONMARK"}
 INS_PUNCTS = {",": ",COMMA", ";": ";SEMICOLON", ":": ":COLON", "-": "-DASH"}
 
 forbidden_symbols = re.compile(r"[\[\]\(\)\/\\\>\<\=\+\_\*]")
 numbers = re.compile(r"\d")
-multiple_punct = re.compile(r'([\.\?\!\,\:\;\-])(?:[\.\?\!\,\:\;\-]){1,}')
+multiple_punct = re.compile(r"([\.\?\!\,\:\;\-])(?:[\.\?\!\,\:\;\-]){1,}")
 
 is_number = lambda x: len(numbers.sub("", x)) / len(x) < 0.6
 
+
 def untokenize(line):
     return line.replace(" ruv ", "")
+
 
 def process_line(line):
 
